543e15fb8d9e00b791b09977c622a1cc34826920
marco rojouan <marcoprojouan@outlook.fr>
2024-11-07 16:50:10 +0100

    feat: pseudoform components with style and fonts

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import PseudoForm from "../components/PseudoForm";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <PseudoForm />
+      </section>
+    </main>
+  );
+}
