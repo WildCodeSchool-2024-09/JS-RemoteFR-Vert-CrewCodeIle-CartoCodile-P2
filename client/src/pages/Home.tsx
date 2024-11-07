2bdc6737307f6d5f9a7ee0630dbc065253925487
Baczkiewicz Damien <baczkiewicz.damien@hotmail.fr>
2024-11-07 17:04:09 +0100

    feat: create rules component with style

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Rules from "../components/Rules";
+
+const Home = () => {
+  return (
+    <main>
+      <section>
+        <Rules />
+      </section>
+    </main>
+  );
+};
+
+export default Home;
