692acfb22dac4a5c1aa5ce60a20d95d5739ec0d0
Mathilde_Glet <mathilde_glet78@orange.fr>
2024-11-11 13:24:14 +0100

    feat: finished menuBurger

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import "./index.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Home />
+      <Header />
+      <Footer />
     </>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,3 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-
-      <Footer />
-    </>
-  );
+  return <></>;
 }
