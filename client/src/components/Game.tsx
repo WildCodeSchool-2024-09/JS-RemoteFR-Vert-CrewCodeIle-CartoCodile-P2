62285ea5055d8b24ffe3ad250cf08bfd9fba20ca
Baczkiewicz Damien <baczkiewicz.damien@hotmail.fr>
2024-11-19 22:42:27 +0100

    feat: add colors for right and wrong answers and change in question generation

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,49 +1,55 @@
 import { useEffect, useState } from "react";
-import type { Country, Question } from "./lib/definitions";
+import { useNavigate } from "react-router-dom";
+import type { Country, Question } from "../lib/definitions";
 
 export default function Game() {
-  const [question, setQuestion] = useState<Question | null>(null);
-  const [answers, setAnswers] = useState<string[]>([]);
-  const [hint, setHint] = useState<string>("");
-
-  const [openHint, setOpenHint] = useState("invisible");
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+  const [openHint, setOpenHint] = useState<string>("invisible");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    Promise.all([
-      fetch("http://localhost:3310/api/countries").then((res) => {
-        return res.json();
-      }),
-      fetch("http://localhost:3310/api/badcountries").then((res) => {
-        return res.json();
-      }),
-    ])
-      .then(([countriesData, badCountriesData]) => {
-        generateQuestion(countriesData, badCountriesData);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
+    fetchQuestions();
   }, []);
 
-  // Mélanger les réponses
-  const shuffleArray = (array: string[]): string[] => {
-    return array.sort(() => 0.5 - Math.random());
+  const fetchQuestions = () => {
+    Promise.all([
+      fetch("http://localhost:3310/api/countries").then((res) => res.json()),
+      fetch("http://localhost:3310/api/badcountries").then((res) => res.json()),
+    ])
+      .then(([countriesData, badCountriesData]) => {
+        const generatedQuestions = generateAllQuestions(
+          countriesData,
+          badCountriesData,
+        );
+        setQuestions(generatedQuestions);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   };
 
-  const generateQuestion = (countries: Country[], badcountries: Country[]) => {
-    const randomCountry =
-      countries[Math.floor(Math.random() * countries.length)];
-    const questionType = getQuestionType(randomCountry);
-    const countryHint = randomCountry.hint;
-    const correctAnswer = randomCountry.countryName;
-    const badAnswers = getRandomBadCountries(badcountries);
-    const allAnswers = shuffleArray([correctAnswer, ...badAnswers]);
+  const generateAllQuestions = (
+    countries: Country[],
+    badCountries: Country[],
+  ) => {
+    const questions: Question[] = [];
+    for (let i = 0; i < 5; i++) {
+      const randomCountry =
+        countries[Math.floor(Math.random() * countries.length)];
+      const questionType = getQuestionType(randomCountry);
+      const correctAnswer = randomCountry.countryName;
+      const badAnswers = getRandomBadCountries(badCountries);
+      const allAnswers = shuffleArray([correctAnswer, ...badAnswers]);
 
-    setQuestion({
-      id: randomCountry.id,
-      country: randomCountry,
-      type: questionType,
-    });
-    setAnswers(allAnswers);
-    setHint(countryHint);
+      questions.push({
+        id: randomCountry.id,
+        country: randomCountry,
+        type: questionType,
+        answers: allAnswers,
+        hint: randomCountry.hint,
+      });
+    }
+    return questions;
   };
 
   const getQuestionType = (country: Country) => {
@@ -54,32 +60,59 @@ export default function Game() {
     return questionTypes[Math.floor(Math.random() * questionTypes.length)];
   };
 
-  // Fonction pour avoir 3 mauvaises réponses dans le QCM
-  const getRandomBadCountries = (badcountries: Country[]) => {
-    const shuffledBadCountries = badcountries.sort(() => 0.5 - Math.random());
+  const getRandomBadCountries = (badCountries: Country[]) => {
+    const shuffledBadCountries = badCountries.sort(() => 0.5 - Math.random());
     const randomBadCountries = shuffledBadCountries.slice(0, 3);
     return randomBadCountries.map((country) => country.countryName);
   };
 
-  if (!question) {
+  const shuffleArray = (array: string[]): string[] => {
+    return array.sort(() => 0.5 - Math.random());
+  };
+
+  const handleAnswerClick = (answer: string) => {
+    setSelectedAnswer(answer);
+  };
+
+  const handleNextQuestionClick = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+      setSelectedAnswer(null);
+      setOpenHint("invisible");
+    } else {
+      navigate("/");
+    }
+  };
+
+  const getButtonClass = (answer: string) => {
+    if (!selectedAnswer) return "bg-primary text-secondary";
+    if (answer === currentQuestion.country.countryName) return "bg-green-500";
+    return "bg-red-500";
+  };
+
+  const handleHintClick = () => {
+    setOpenHint(openHint === "visible" ? "invisible" : "visible");
+  };
+
+  if (questions.length === 0) {
     return <div>Chargement des valises...</div>;
   }
 
-  const handleClick = () => {
-    openHint === "visible" ? setOpenHint("invisible") : setOpenHint("visible");
-  };
+  const currentQuestion = questions[currentQuestionIndex];
 
   return (
     <div className="flex flex-col p-4 items-center">
-      <h2 className="invisible">Type de question: {question.type.label}</h2>
+      <h2 className="invisible">
+        Type de question: {currentQuestion.type.label}
+      </h2>
       <section className="font-Koulen text-lg">
-        {question.type.label === "capitale" ? (
-          <p>{question.country.capital}</p>
+        {currentQuestion.type.label === "capitale" ? (
+          <p>{currentQuestion.country.capital}</p>
         ) : (
           <img
             className="resized"
-            src={question.type.image || ""}
-            alt={question.type.label}
+            src={currentQuestion.type.image || ""}
+            alt={currentQuestion.type.label}
           />
         )}
       </section>
@@ -89,26 +122,37 @@ export default function Game() {
             Dans quel pays nous situons nous ?
           </h1>
         </section>
-        <button type="button" onClick={handleClick} className="visible">
-          <p>
-            <img
-              className="pt-2 self-center w-6 m-auto"
-              src="\public\images\indice (1).png"
-              alt="Indice"
-            />
-          </p>
-          <p className={openHint}>Indice: {hint}</p>
+        <button type="button" className="visible" onClick={handleHintClick}>
+          <img
+            className="pt-2 self-center w-6 m-auto"
+            src="\public\images\indice (1).png"
+            alt="Indice"
+          />
+          <p className={openHint}>Indice: {currentQuestion.hint}</p>
         </button>
         <div className="flex flex-col gap-2">
-          {answers.map((answer) => (
+          {currentQuestion.answers.map((answer) => (
             <button
-              className="bg-primary text-secondary rounded-lg h-10 uppercase"
+              className={`rounded-lg h-10 uppercase ${getButtonClass(answer)}`}
               type="button"
               key={answer}
+              disabled={!!selectedAnswer}
+              onClick={() => handleAnswerClick(answer)}
             >
               {answer}
             </button>
           ))}
+        </div>
+        <div className="flex justify-center mt-4">
+          <button
+            className="text-secondary font-Koulen bg-accent rounded p-2"
+            type="button"
+            onClick={handleNextQuestionClick}
+          >
+            {currentQuestionIndex === questions.length - 1
+              ? "Retour à l'accueil"
+              : "Question suivante"}
+          </button>
         </div>
       </section>
     </div>
